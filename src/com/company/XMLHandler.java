7f9344dce5c15812557c5fdc75f48868b655c888
BBence43 <messi.bbn@gmail.com>
2022-06-07 19:22:24 +0200

    projekt

--- src/com/company/XMLHandler.java
+++ src/com/company/XMLHandler.java
@@ -2,6 +2,8 @@ package com.company;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -212,31 +214,160 @@ public class XMLHandler {
         System.out.println("Sikeres mentés!");
     }
 
+    public void FileLoadUgyfel(){
+        //read
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document document = db.parse(new File("ugyfelek.xml"));
+
+            NodeList list = document.getElementsByTagName("Ugyfel");
+            manageNodeUgyfel(list);
+            System.out.println("Sikeres betöltés!");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    //Ez a metódus felel a Nodelistbe előzőlegesen betöltött adatok betöltéséért az Ugyfel Classba, vagyis az adatokat betöltjük a programba az XML fájlból.
+    private static void manageNodeUgyfel(NodeList nodeList) {
+        for (int count = 0;count < nodeList.getLength();count++){
+            Node tmpNode = nodeList.item(count);
+            if (tmpNode.getNodeType() == Node.ELEMENT_NODE){
+                Element element = (Element) tmpNode;
+                String name = element.getElementsByTagName("name").item(0).getTextContent();
+                String lakcim = element.getElementsByTagName("lakcim").item(0).getTextContent();
+                String anyjaneve = String.valueOf(element.getElementsByTagName("anyjaneve").item(0).getTextContent());
+                String email = String.valueOf(element.getElementsByTagName("email").item(0).getTextContent());
+                String szuletesidatum = String.valueOf(element.getElementsByTagName("szuletesidatum").item(0).getTextContent());
+                Integer id = Integer.valueOf(element.getElementsByTagName("id").item(0).getTextContent());
+                Ugyfel ugyfel = new Ugyfel(id,name,lakcim,anyjaneve,email,szuletesidatum);
+                Ugyfel.lista.add(ugyfel);
+            }
+        }
+    }
+
+    public void FileLoadAuto(){
+        //read
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document document = db.parse(new File("auto.xml"));
+
+            NodeList list = document.getElementsByTagName("Car");
+            manageNodeUgyfel(list);
+            System.out.println("Sikeres betöltés!");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    //Ez a metódus felel a Nodelistbe előzőlegesen betöltött adatok betöltéséért az AutoInsurance Classba, vagyis az adatokat betöltjük a programba az XML fájlból.
+    private static void manageNodeAuto(NodeList nodeList) {
+        for (int count = 0;count < nodeList.getLength();count++){
+            Node tmpNode = nodeList.item(count);
+            if (tmpNode.getNodeType() == Node.ELEMENT_NODE){
+                Element element = (Element) tmpNode;
+                Boolean tonna =Boolean.valueOf(element.getElementsByTagName("tonna").item(0).getTextContent()) ;
+                String rendszam = element.getElementsByTagName("rendszam").item(0).getTextContent();
+                String gyartasiev = String.valueOf(element.getElementsByTagName("gyartasiev").item(0).getTextContent());
+                Integer teljesitmeny = Integer.valueOf(element.getElementsByTagName("email").item(0).getTextContent());
+                Integer id = Integer.valueOf(element.getElementsByTagName("id").item(0).getTextContent());
+                AutoInsurance auto = new AutoInsurance(id,tonna,rendszam,gyartasiev,teljesitmeny);
+                AutoInsurance.lista.add(auto);
+            }
+        }
+    }
+
+    public void FileLoadProperty(){
+        //read
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document document = db.parse(new File("property.xml"));
+
+            NodeList list = document.getElementsByTagName("Property");
+            manageNodeUgyfel(list);
+            System.out.println("Sikeres betöltés!");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    //Ez a metódus felel a Nodelistbe előzőlegesen betöltött adatok betöltéséért az AutoInsurance Classba, vagyis az adatokat betöltjük a programba az XML fájlból.
+    private static void manageNodeProperty(NodeList nodeList) {
+        for (int count = 0;count < nodeList.getLength();count++){
+            Node tmpNode = nodeList.item(count);
+            if (tmpNode.getNodeType() == Node.ELEMENT_NODE){
+                Element element = (Element) tmpNode;
+                String biztaddress =element.getElementsByTagName("biztaddress").item(0).getTextContent() ;
+                IngatlanType ingatlantype =IngatlanType.valueOf(element.getElementsByTagName("ingatlantype").item(0).getTextContent()) ;
+                Integer mellekepuletek = Integer.valueOf(element.getElementsByTagName("mellekepuletek").item(0).getTextContent());
+                Boolean lakottsag = Boolean.valueOf(element.getElementsByTagName("lakottsag").item(0).getTextContent());
+                Integer id = Integer.valueOf(element.getElementsByTagName("id").item(0).getTextContent());
+                LakasInsurance lakas = new LakasInsurance(id,biztaddress,ingatlantype,mellekepuletek,lakottsag);
+                LakasInsurance.lista.add(lakas);
+            }
+        }
+    }
+    public void FileLoadLife(){
+        //read
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document document = db.parse(new File("life.xml"));
+
+            NodeList list = document.getElementsByTagName("Life");
+            manageNodeUgyfel(list);
+            System.out.println("Sikeres betöltés!");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    //Ez a metódus felel a Nodelistbe előzőlegesen betöltött adatok betöltéséért az AutoInsurance Classba, vagyis az adatokat betöltjük a programba az XML fájlból.
+    private static void manageNodeLife(NodeList nodeList) {
+        for (int count = 0;count < nodeList.getLength();count++){
+            Node tmpNode = nodeList.item(count);
+            if (tmpNode.getNodeType() == Node.ELEMENT_NODE){
+                Element element = (Element) tmpNode;
+                Boolean fogyatekos = Boolean.valueOf(element.getElementsByTagName("fogyatekos").item(0).getTextContent()) ;
+                Integer eletkor =Integer.valueOf(element.getElementsByTagName("eletkor").item(0).getTextContent());
+                Boolean kronikus = Boolean.valueOf(element.getElementsByTagName("kronikus").item(0).getTextContent());
+                Boolean halalos = Boolean.valueOf(element.getElementsByTagName("halalos").item(0).getTextContent());
+                Integer id = Integer.valueOf(element.getElementsByTagName("id").item(0).getTextContent());
+                EletInsurance life = new EletInsurance(id,fogyatekos,eletkor,kronikus,halalos);
+                EletInsurance.lista.add(life);
+            }
+        }
+    }
+
+
 
 
 
     public void FileChecker(){
         File fileugyfel = new File("ugyfelek.xml");
         if (fileugyfel.exists()){
-            FileLoad();
+            FileLoadUgyfel();
         }else{
             WriteUgyfel();
         }
         File fileauto = new File("auto.xml");
         if (fileugyfel.exists()){
-            FileLoad();
+            FileLoadAuto();
         }else{
             WriteAuto();
         }
         File fileproperty = new File("property.xml");
         if (fileproperty.exists()){
-            FileLoad();
+            FileLoadProperty();
         }else{
             WriteLakas();
         }
         File filelife = new File("life.xml");
         if (filelife.exists()){
-            FileLoad();
+            FileLoadLife();
         }else{
             WriteElet();
         }
